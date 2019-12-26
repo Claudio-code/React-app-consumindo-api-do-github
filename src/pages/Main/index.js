827b01c9ab3f246bf1f41ef1431013c8af2dd8d8
claudio2424 <suporte2424@outlook.com>
2019-12-26 06:17:18 -0300

    feita alterações de estilo na pagina principal

--- src/pages/Main/index.js
+++ src/pages/Main/index.js
@@ -1,9 +1,41 @@
-import React from "react";
+import React, { useState } from "react";
 
-import { FaGithubAlt, FaPlus } from "react-icons/fa";
+import { FaGithubAlt, FaPlus, FaSpinner } from "react-icons/fa";
 import { Container, Form, SubmitButton } from "./styles";
+import api from "../../services/api";
 
 export default function Main() {
+  const [newRepo, setNewRepo] = useState("");
+  const [repositorios, setRepositorios] = useState([]);
+  const [loading, setLoading] = useState(false);
+
+  async function handleSubmit(event) {
+    event.preventDefault();
+    try {
+      if (!newRepo) {
+        return;
+      }
+      setLoading(true);
+
+      const response = await api.get(`/repos/${newRepo}`);
+
+      const data = {
+        name: response.data.full_name,
+      };
+
+      const repositoriosPlus = repositorios;
+      repositoriosPlus.push(data);
+
+      setRepositorios(repositoriosPlus);
+      setNewRepo("");
+      setLoading(false);
+    } catch (error) {
+      console.log(error);
+      setNewRepo("");
+      setLoading(false);
+    }
+  }
+
   return (
     <Container>
       <h1>
@@ -11,11 +43,20 @@ export default function Main() {
         Repositorios
       </h1>
 
-      <Form onSubmit={() => {}}>
-        <input type="text" placeholder="Adicionar o repositorio" />
+      <Form onSubmit={handleSubmit}>
+        <input
+          type="text"
+          placeholder="Adicionar o repositorio"
+          value={newRepo}
+          onChange={newValue => setNewRepo(newValue.target.value)}
+        />
 
-        <SubmitButton>
-          <FaPlus color="#fff" fontSize={14} />
+        <SubmitButton loading={loading}>
+          {loading ? (
+            <FaSpinner color="#000" fontSize={14} />
+          ) : (
+            <FaPlus color="#fff" fontSize={14} />
+          )}
         </SubmitButton>
       </Form>
     </Container>
--- src/pages/Main/styles.js
+++ src/pages/Main/styles.js
@@ -1,4 +1,4 @@
-import styled from "styled-components";
+import styled, { keyframes, css } from "styled-components";
 
 export const Container = styled.div`
   max-width: 700px;
@@ -17,6 +17,15 @@ export const Container = styled.div`
     svg {
       margin-right: 10px;
     }
+  }
+`;
+
+const rotate = keyframes`
+  from {
+    transform: rotate(0deg);
+  }
+  to {
+    transform: rotate(360deg);
   }
 `;
 
@@ -39,9 +48,10 @@ export const Form = styled.form`
   }
 `;
 
-export const SubmitButton = styled.button.attrs({
+export const SubmitButton = styled.button.attrs(props => ({
   type: "submit",
-})`
+  disabled: props.loading,
+}))`
   background: #01579b;
   border: 0;
   padding: 0 15px;
@@ -52,4 +62,17 @@ export const SubmitButton = styled.button.attrs({
   display: flex;
   justify-content: center;
   align-items: center;
+
+  &[disabled] {
+    background: #bdbdbd;
+    cursor: not-allowed;
+  }
+
+  ${props =>
+    props.loading &&
+    css`
+      svg {
+        animation: ${rotate} 2s linear infinite;
+      }
+    `}
 `;

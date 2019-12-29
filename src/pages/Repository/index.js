1b7613bb8b7b875ecaa22ace5454eb9f414e6b84
claudio2424 <suporte2424@outlook.com>
2019-12-29 12:53:37 -0300

    pegando informacoes de cada repositorio com suas issues

--- src/pages/Repository/index.js
+++ src/pages/Repository/index.js
@@ -1,7 +1,37 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 
+import api from "../../services/api";
 // import { Container } from './styles';
 
-export default function Repository() {
+export default function Repository({ match }) {
+  const [loading, setLoading] = useState(true);
+  const [allInfoRepository, setAllInfoRepository] = useState({});
+  const [issuesIsRepository, setIssuesIsRepository] = useState([]);
+  const [repository] = useState(
+    decodeURIComponent(match.params.repositoryName)
+  );
+
+  useEffect(() => {
+    async function getAllInfoRepository() {
+      try {
+        const [repositoryInfo, issues] = await Promise.all([
+          api.get(`/repos/${repository}`),
+          api.get(`/repos/${repository}/issues`, {
+            params: {
+              state: "open",
+              per_page: 7,
+            },
+          }),
+        ]);
+        setAllInfoRepository(repositoryInfo.data);
+        setIssuesIsRepository(issues.data);
+        setLoading(false);
+      } catch (error) {
+        console.log(error);
+      }
+    }
+    getAllInfoRepository();
+  }, [repository]);
+
   return <div>Repository</div>;
 }
